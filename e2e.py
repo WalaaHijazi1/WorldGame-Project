d79651939cd1aa04175a83695e6be2bb3b100d6e
WalaaHijazi1 <walaa.25.11@hotmail.com>
2025-04-07 22:14:24 +0300

    updated e2e 1...

--- e2e.py
+++ e2e.py
@@ -1,9 +1,10 @@
-
 #This file will have two functions.
 #Functions
 #1. test_scores_service - it’s purpose is to test our web service. It will get the application
 #URL as an input, open a browser to that URL, select the score element in our web page,
 #check that it is a number between 1 to 1000 and return a boolean value if it’s true or not.
+
+# -*- coding: utf-8 -*-
 
 
 from selenium import webdriver
