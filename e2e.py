d5d56531f25ad6c6363db3f226c8a9cb660df3f3
WalaaHijazi1 <walaa.25.11@hotmail.com>
2025-04-08 18:02:46 +0300

    e2e updated ...

--- e2e.py
+++ e2e.py
@@ -14,6 +14,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import sys
 
@@ -21,33 +23,38 @@ import os
 import requests
 
 
-options = Options()
-
-# Set Chrome options
-# Options available in Selenium that help in testing in the background, disabling extensions, etc.
-# It's an object that let the web driver customize the behavior of a web browser.
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument(f"--user-data-dir={os.path.expanduser('~')}/chrome_data")
-options.add_argument("--remote-debugging-port=9222")
-
-
-# Set up the ChromeDriver Service
-chromedriver_path = "/root/.wdm/drivers/chromedriver/linux64/134.0.6998.88/chromedriver-linux64/chromedriver"
-service = Service(executable_path=chromedriver_path)
-
-# Initialize the Chrome WebDriver with the service and options
-driver = webdriver.Chrome(service=service, options=options)
 
 def test_scores_service(url):
+    options = Options()
 
-    driver.get(url)
+    # Set Chrome options
+    # Options available in Selenium that help in testing in the background, disabling extensions, etc.
+    # It's an object that let the web driver customize the behavior of a web browser.
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument(f"--user-data-dir={os.path.expanduser('~')}/chrome_data")
+    options.add_argument("--remote-debugging-port=9222")
+
+
+    # Set up the ChromeDriver Service
+    chromedriver_path = "/root/.wdm/drivers/chromedriver/linux64/134.0.6998.88/chromedriver-linux64/chromedriver"
+    service = Service(executable_path=chromedriver_path)
+
+    # Initialize the Chrome WebDriver with the service and options
+    driver = webdriver.Chrome(service=service, options=options)
+
+
 
     try:
 
+        driver.get(url)
+
         # Wait for the page to load
         time.sleep(2)
+        
+        # Explicitly wait for the score element to be visible
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'score')))
 
         #Find the score element:
         score_element = driver.find_element(By.ID, 'score')
