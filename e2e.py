e6c419203687c5fb2d7c859a4efab5149027651c
WalaaHijazi1 <walaa.25.11@hotmail.com>
2025-04-30 17:00:20 +0300

    importing missing lib ...

--- e2e.py
+++ e2e.py
@@ -12,6 +12,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 import sys
 import os
