e5746d8063799487ccfd67582f0167513dcec8ad
WalaaHijazi1 <walaa.25.11@hotmail.com>
2025-05-01 13:52:42 +0300

    updating testing run ...

--- e2e.py
+++ e2e.py
@@ -43,7 +43,11 @@ def automated_game_test(url):
         wait.until(EC.element_to_be_clickable((By.ID, "startBtn"))).click()
 
         # Step 5: Wait for result
-        result_element = wait.until(EC.presence_of_element_located((By.ID, "resultMessage")))
+        try:
+            result_element = wait.until(EC.presence_of_element_located((By.ID, "resultMessage")))
+        except:
+            result_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "message")))
+        #result_element = wait.until(EC.presence_of_element_located((By.ID, "resultMessage")))
         result_text = result_element.text
         print(f"\nResult message: {result_text}")
 
