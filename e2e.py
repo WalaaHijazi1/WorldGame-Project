b1f086211b48f5f003b9b8dd4305771ad8b36cad
WalaaHijazi1 <walaa.25.11@hotmail.com>
2025-05-01 14:24:19 +0300

    updating testing run ...

--- e2e.py
+++ e2e.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import re
+import time
 import traceback
 
 def automated_game_test(url):
